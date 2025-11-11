07d2e0cdf29adc6ceea91d3c5bec11553ac21d94
Ali-Hammoud-12 <alihammoudah12@gmail.com>
2025-11-11 20:36:02 +0200

    lambda construct

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -2,5 +2,9 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { CdkStack } from '../lib/cdk-stack';
 
+// App (the whole CDK project)
+//  └── Stack (a CloudFormation deployment unit)
+//        └── Function (an AWS resource inside that stack)
+
 const app = new cdk.App();
 new CdkStack(app, 'CdkStack');
--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,11 +1,17 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib/core';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+
+// the class constructors of both CdkWorkshopStack and lambda.Function (and many other classes in the CDK) have the signature (scope, id, props)
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const hello = new Function(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_22_X,
+      handler: "hello.handler",
+      code: Code.fromAsset("functions/hello"),
+    });
   }
 }
